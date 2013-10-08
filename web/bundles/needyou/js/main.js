4f7ffe823179fa7fc9e6ac06c256ecabf78764f6
Baurzhan Makhambetov <baurmatic@gmail.com>
2013-10-08 18:45:16 +0600

    js and css code more organized now

--- web/bundles/needyou/js/main.js
+++ web/bundles/needyou/js/main.js
@@ -1,9 +1,64 @@
 (function($){
 
-$(document).ready(function(){
+    $.jAvatarUploadable = function(){
+          var jau = {
+             animated: true,
+             toggleOverlay: function(){
+                if($('#over_x').css('display') == 'none') {
+                   $('#over_x').css('display',"block");
+                   $('#over_y').css('display',"block");
+                   $('#mybutt').css('display',"block");
+                }else{
+                   $('#over_x').css('display',"none");
+                   $('#over_y').css('display',"none");
+                   $('#preview').css('display',"none");
+                   $('#upload_info').css('display','none');
+                }                
+             },
+             doLoad: function(){
+                   document.getElementById("mybutt").style.display='none';
+                   var form = new FormData(document.getElementById("upload_form"));
+                   var oFile = document.getElementById('image').files[0];
+                   var oImage = document.getElementById('preview');
+                   var upload_info = document.getElementById('upload_info');
+                   var oReader = new FileReader();
+                   oReader.onload = function(e){
+                       // e.target.result contains the DataURL which we will use as a source of the image
+                       oImage.src = e.target.result;
+                       oImage.style.display="block";
+                       upload_info.style.display="block";
 
-    //alert($('#image').val());
-       /*$("#login").submit(function(event){
+
+                       oImage.onload = function () { // binding onload event
+                           // we are going to display some custom image information here
+                           sResultFileSize = bytesToSize(oFile.size);
+                           document.getElementById('fileinfo').style.display = 'block';
+                           document.getElementById('filename').innerHTML = 'Name: ' + oFile.name;
+                           document.getElementById('filesize').innerHTML = 'Size: ' + sResultFileSize;
+                           document.getElementById('filetype').innerHTML = 'Type: ' + oFile.type;
+                           document.getElementById('filedim').innerHTML = 'Dimension: ' + oImage.naturalWidth + ' x ' + oImage.naturalHeight;
+                       };
+                   };
+
+                   oReader.readAsDataURL(oFile);
+                   form.append("userfile",oFile);
+                   var oReq = new XMLHttpRequest();
+                   oReq.open("POST","/upload",true);
+                   oReq.onload=function(oEvent){
+                   }
+                    oReq.send(form); 
+               }              
+             };
+          
+          return {
+                    toggleOverlay: jau.toggleOverlay,
+                    doLoad: jau.doLoad,
+          };
+    }
+
+    $(document).ready(function(){
+
+       $("#login").submit(function(event){
            this.form.submit();
             event.preventDefault();
        });
@@ -13,71 +68,14 @@ $(document).ready(function(){
                    this.form.submit();
                }
            });
-       });*/
-        /*$('#button').on('click',
-            function(){
-                aButtonPressed();
-            }
-        );*/
+       });
         $('#friends').keydown(function(){
                 showFriends();
         });
-        $('#photo').on('click',
-            function(){
-                $('#over_x').css('display',"block");
-                $('#over_y').css('display',"block");
-            });
-        $('#photo_close').on('click',
-            function(){
-                $('#over_x').css('display',"none");
-                $('#over_y').css('display',"none");
-            }
-        );
-
-
-//        $('#upload').on('change',
-//            function(){
-//                var form = new FormData($('#upload'));
-//                alert(form);
-//                $.ajax({
-//                    url: '/upload',
-//                    type: 'POST',
-//                    success: function (r) {
-//                        //$('.container').html(r);
-//                        var response = JSON.parse(r);
-//                        alert(response.data);
-//                    },
-//                    data: form,
-//                    cache: false,
-//                    contentType: false,
-//                    processData: false
-//                });
-//                /*$.post('upload',
-//                {data: form},
-//                    function(response){
-//                        alert(response.data);
-//                    }, "json");*/
-//            });
-//
-
-        $('#image').on('change',function(){
-             doLoad();
-            /*var form = new FormData($('#myform')[0]);
-            //alert($("#myform")[2]);
-            $.ajax({
-                url: '/upload',
-                type: 'POST',
-                success: function (r) {
-                    //$('.container').html(r);
-                    var response = JSON.parse(r);
-                    alert(response.data);
-                },
-                data: form,
-                cache: false,
-                contentType: false,
-                processData: false
-            });*/
-        });
-
+        
 });
+
+
 })(jQuery);
+
+
